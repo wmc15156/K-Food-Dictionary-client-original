151561133ac8480400f99f6dff7d62e7acf838bb
Hocheol <samiosae@gmail.com>
2020-08-13 14:17:44 +0900

    feat: 콤포넌트 구조 변경
    네비게이션 바에서 전반적인 대분류를 선택할 수 있게끔
    컴포넌트를 변경합니다.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import './App.css';
-
+import img from './images/logo.jpg'
+import { BrowserRouter } from 'react-router-dom'
+import Nav from './pages/Nav'
+import Main from './pages/Main';
 class App extends React.Component {
   render() {
     return (
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <img className="mainPageLogo" src={img} alt="logo"></img>
+        <Nav></Nav>
+
+        <Main></Main>
+      </BrowserRouter>
+
     );
   }
 }
-
 export default App;
