f4873b5e5357909472e276e0349f39ff5a316b78
rladbgus <cocokiuuu1858@gmail.com>
2020-08-17 12:16:30 +0900

    add: mypage속 admin버튼 클릭시 adminpage로 redirect

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,22 +1,57 @@
+/* eslint-disable react/prop-types */
 import React from "react";
+import { withRouter } from "react-router-dom";
+import axios from "axios";
 
-// 이 페이지는 mypage 입니다.
-// 아직 기능 구현 전이라 임의의 레이아웃만 짜놨습니다.
-// 로그인 상태에서 mypage 이동 시 해당 유저의 인포와 찜 리스트를 서버에서 받아와야 합니다.
-// 로그인 안되 있는 상태에서 mypage 들어올 시 로그인 안되어있다는 안내가 있으면 좋을 것 같습니다.
+axios.defaults.withCredentials = true;
+
+// 로그인 상태에서 mypage 이동 시 해당 찜 리스트 서버에서 받아오기
+// 로그인 안되 있는 상태에서 mypage 들어올 시 로그인 안되어있다는 안내 후 로그인페이지로 
 
 class Mypage extends React.Component {
+
     render() {
+        function AdminPage(e) {
+            e.preventDefault();
+            window.location = 'http://localhost:3000/admin';
+        }
 
         return (
             <div>
                 <h1>Mypage</h1>
-                <div className="userInfo">유저 정보입니다</div>
-                <div className="favoriteFood">찜한 음식입니다</div>
+                <form
+                    onSubmit={e => {
+                        e.preventDefault();
+                        axios
+                            .post('http://3.34.193.46:5000/user/logout', this.state)
+                            .then(res => {
+                                if (res.status === 302) {
+                                    alert('로그아웃이 완료되었습니다')
+                                    this.props.history.push('/login')
+                                }
+                            })
+                            .catch(err =>
+                                console.log(err),
+                            );
+                    }}
+                >
+
+                    <div className="mypageBt">
+                        <button className="adminBt" onClick={AdminPage}>
+                            Admin
+                    </button>
+                        <button className="logoutBt" type="submit">
+                            Logout
+                     </button>
+                    </div>
+
+                    <div className="userInfo">유저 정보입니다</div>
+                    <div className="favoriteFood">찜한 음식입니다</div>
+                </form>
             </div>
         )
     }
 }
 
 
-export default Mypage;
+export default withRouter(Mypage);
