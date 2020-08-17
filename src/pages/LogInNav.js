8d40afd9be4f66324b9e5d90e992abf3bb331a2b
Hocheol <samiosae@gmail.com>
2020-08-17 19:11:32 +0900

    추가: 로그인시 보여지는 네비게이션바
    어드민페이지, 마이페이지 버튼이 보이고
    로그인, 회원가입 버튼은 보이지 않는다.

---
+++ src/pages/LogInNav.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import img from "../images/logo.jpg";
+
+class LogInNav extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="NavBar">
+                    <Link to="/admin" className="link">Admin</Link>
+                    <Link to="/mypage" className="link">Mypage</Link>
+                    <Link to="/" className="homelink">
+                        <img className="mainPageLogo" src={img} alt="logo"></img>
+                    </Link>
+                </div>
+            </div >
+        );
+    }
+}
+
+export default LogInNav;
