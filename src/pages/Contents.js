6a97edc22a2a3d04b9032e800a600843b3e6f687
Gelo <59912499+bbirds94@users.noreply.github.com>
2020-08-18 18:08:56 +0900

    Contents 초기화 (유투브 에러로 인한)

--- src/pages/Contents.js
+++ src/pages/Contents.js
@@ -1,64 +1,12 @@
 import React from "react";
-import VideoPlayer from "./ContentsPageList/VideoPlayer";
-import VideoList from "./ContentsPageList/VideoList";
-import { searchYouTube } from "../pages/ContentsPageList/searchYouTube";
-import { YOUTUBE_API_KEY } from "../config/youtube";
+
 
 class Contents extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            videos: [],
-            currentVideo: null
-        };
-    }
-
-    componentDidMount() {
-        this.getYouTubeVideos("먹방");
-    }
-
-    getYouTubeVideos(query) {
-        var options = {
-            key: YOUTUBE_API_KEY,
-            query: query
-        };
-
-        searchYouTube(options, videos =>
-            this.setState({
-                videos: videos,
-                currentVideo: videos[0]
-            })
-        );
-    }
-
-    handleVideoListClick(video) {
-        this.setState({
-            currentVideo: video
-        });
-    }
-
     render() {
         return (
-            <div>
-                <div>
-                    <VideoPlayer video={this.state.currentVideo} />
-                </div>
-
-                <div className="favoriteBox">
-                    음식 정보
-                </div>
-
-                <div>
-                    <VideoList
-                        handleVideoListClick={this.handleVideoListClick.bind(
-                            this
-                        )}
-                        videos={this.state.videos}
-                    />
-                </div>
-            </div>
+            <div>Contents Page</div>
         )
     }
 }
-export default Contents;
+
+export default Contents
