4b5b71fcab3516e83af16a17af926cf03c73022b
Hocheol <samiosae@gmail.com>
2020-08-18 16:40:03 +0900

    수정 : 브라우저라우터 적용 및 Not Found컴포넌트 추가
    인덱스 페이지에 있던 브라우저 라우터를 app.js 컴포넌트 단으로 내려왔습니다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { Switch, Route, useHistory, Redirect, Router } from 'react-router-dom';
+import { Switch, Route, useHistory, Redirect, Router, BrowserRouter } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages//Signup';
 import Mypage from './pages//Mypage';
@@ -11,7 +11,9 @@ import Home from "./pages/Home"
 import SeaList from "./pages/MainPageList/SeaList"
 import MeatList from "./pages//MainPageList/MeatList"
 import DessertList from "./pages//MainPageList/DessertList"
-import OnboardNav from "./pages/OnboardNav"
+import OnboardNav from "./pages/OnboardNav";
+import NotFound from "./pages/NotFound";
+import LogOut from "./pages/LogOut";
 
 // 로그인 시에만 admin 페이지로 갈 수 있는 조건이 필요합니다.
 
@@ -54,42 +56,31 @@ class App extends React.Component {
     const { isLogin, userinfo } = this.state;
     console.log(isLogin, userinfo, '로그인 여부와 유저 인포');
     return (
-      <div>
-        <div>
-          <OnboardNav isLogin={this.state.isLogin}></OnboardNav>
-        </div>
+      <BrowserRouter>
+        <OnboardNav isLogin={isLogin}></OnboardNav>
         <div className="mainPageBox">
-
           <Switch>
-
             <Route exact path="/login"
               render={() => (<Login isLogin={isLogin}
                 handleIsLoginChange={this.handleIsLoginChange.bind(this)} />)}
             />
-
             <Route exact path="/signup"
               render={() => <Signup isLogin={isLogin} />}
             />
-
             <Route exact path="/mypage"
               render={() => <Mypage isLogin={isLogin} userinfo={userinfo} />}
             />
-
+            <Route exact path="/logout" render={() => <LogOut isLogin={isLogin} />} />
             <Route exact path="/admin"><Admin /></Route>
-
             <Route exact path="/contents"><Contents /></Route>
-
             <Route exact path="/dessertList"><DessertList /></Route>
-
             <Route exact path="/meatList"><MeatList dish={this.state.foods} /></Route>
-
             <Route exact path="/seafoodList"><SeaList /></Route>
-
             <Route exact path="/"><Home /></Route>
-
+            <Route><NotFound /></Route>
           </Switch>
         </div >
-      </div>
+      </BrowserRouter>
     );
   }
 }
