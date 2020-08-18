cd3a857a2bf03e16315d37da83c5da3417bef600
Hocheol <samiosae@gmail.com>
2020-08-18 16:48:40 +0900

    추가: 로그아웃컴포넌트 추가 로그아웃 컴포넌트를 추가합니다.

---
+++ src/pages/LogOut.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LogOut extends React.Component {
+    render() {
+        return (
+            <h1>
+                로그아웃 되셨습니다.
+            </h1>
+        );
+    }
+}
+
+export default LogOut;
